eb5ea3d8a30403d66a16d40c6c9d58b7974ba23a
Noah-MG <noah.glimcher28@avenues.org>
2025-10-14 16:20:50 -0400

    Rina Learns to Code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/kugo.java
@@ -0,0 +1,32 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@Autonomous(name = "sdoeri")
+public class kugo extends LinearOpMode {
+
+    @Override
+    public void runOpMode(){
+
+        DcMotor FL = hardwareMap.get(DcMotor.class, "FL");
+        DcMotor FR = hardwareMap.get(DcMotor.class, "FR");
+        DcMotor BL = hardwareMap.get(DcMotor.class, "BL");
+        DcMotor BR = hardwareMap.get(DcMotor.class, "BR");
+
+        FL.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        FR.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        BL.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        BR.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        waitForStart();
+
+        FL.setPower(1);
+        FR.setPower(1);
+        BL.setPower(1);
+        BR.setPower(1);
+
+    }
+
+}
