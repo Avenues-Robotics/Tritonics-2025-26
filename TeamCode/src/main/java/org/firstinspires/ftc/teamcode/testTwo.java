9fd175c62b48336e22e40dab43574a984fc189ac
MilindArulraj2028 <milindarulraj@gmail.com>
2025-11-06 11:22:40 -0500

    Began work on an optimal control test

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testTwo.java
@@ -0,0 +1,29 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+@Autonomous(name = "test2")
+public class testTwo extends LinearOpMode {
+
+
+    double b; // "some height"
+    double m; // slop
+    double v; // velocity
+
+    @Override
+
+    public void runOpMode() {
+
+        waitForStart(); // Bro noah is a fool
+
+        // Perform calculations - something  noah cant do
+        double calculation = -b * Math.log(1 + m) + (v / m);
+
+        telemetry.addData("Calculation", calculation); //gpt said addData was better than Println
+        telemetry.update();
+    }
+
+
+
+}
