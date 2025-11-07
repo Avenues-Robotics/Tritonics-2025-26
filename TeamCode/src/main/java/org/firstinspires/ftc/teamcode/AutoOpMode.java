39e4b0e525f4d2ce0842bacdaceea9d66cf89357
Noah-MG <noah.glimcher28@avenues.org>
2025-11-07 09:28:28 -0500

    Started designing auto one.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoOpMode.java
@@ -1,14 +1,24 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
 import org.firstinspires.ftc.teamcode.hardware.DriveTrain;
 
+@Config
 @Autonomous(name = "Tritonics Autonomous")
 public class AutoOpMode extends LinearOpMode {
 
     DriveTrain driveTrain;
+
+    public static double speedOne = 1;
+    public static double distOne = 108;
+    public static double degOne = 55;
+
+    public static double speedTwo = 1;
+    public static double distTwo = 60;
+    public static double degTwo = 0;
 
     @Override
     public void runOpMode() {
@@ -17,6 +27,8 @@ public class AutoOpMode extends LinearOpMode {
 
         waitForStart();
 
-        driveTrain.drive(1, 1000, 0);
+        driveTrain.drive(speedOne, distOne, degOne);
+        driveTrain.drive(speedTwo, distTwo, degTwo);
+        sleep(1000);
     }
 }
