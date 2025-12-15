160fb54371371605d395a5a67bcf683c80f4839f
MilindArulraj2028 <milindarulraj@gmail.com>
2025-12-15 16:57:04 -0500

    Fixed it

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/DriveTrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/DriveTrain.java
@@ -1,7 +1,7 @@
 package org.firstinspires.ftc.teamcode.hardware;
 
 /*
- * Manages drive train hardware including motors and odometry wheels OMG SO COOL DUDE, MY FAVORITE OF THEM
+ * Manages drive train hardware including motors and odometry wheels
  */
 
 import com.acmerobotics.dashboard.FtcDashboard;
