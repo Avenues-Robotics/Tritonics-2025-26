f5029eb1017db07ae947b0e56ebb078183eaabb5
MilindArulraj2028 <milindarulraj@gmail.com>
2025-12-15 17:42:59 -0500

    ecce bahahahahahaha (ut goat)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tasks/Odometry.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tasks/Odometry.java
@@ -6,79 +6,35 @@ import org.firstinspires.ftc.teamcode.hardware.DriveTrain;
 
 public class Odometry extends Task{
 
-    int TotalTicksL;
-    int TotalTicksR;
-    int TotalTicksB;
+    DriveTrain driveTrain;
 
-    int currentTicksL;
-    int currentTicksR;
-    int currentTicksB;
+    double odomLCurr;
+    double odomRCurr;
+    double odomHCurr;
 
-    int storedTicksL;
-    int storedTicksR;
-    int storedTicksB;
+    double odomLSpeed;
+    double odomRSpeed;
+    double odomHSpeed;
 
-    double some_constant;
+    double odomLPrev;
+    double odomRPrev;
+    double odomHPrev;
 
     ElapsedTime dt;
 
-    double distanceL;
-    double distanceR;
-    double distanceB;
-
-    double theta;
-
-    double posDegrees;
-
-    double angle;
-
-    double velocity_X;
-    double velocity_Y;
-
-    double global_X;
-    double global_Y;
-
-    double pos_X;
-    double pos_Y;
-
-    double distBet;
-
-    DriveTrain driveTrain;
-
-    public Odometry() {
-        some_constant = 50.52;
+    public Odometry(DriveTrain driveTrain) {
+        this.driveTrain = driveTrain;
     }
 
     @Override
     public boolean run() {
-        int TotalTicksL = driveTrain.odomL.getCurrentPosition();
-        int TotalTicksR = driveTrain.odomR.getCurrentPosition();
-        int TotalTicksB = driveTrain.odomH.getCurrentPosition();
 
+        odomLCurr = driveTrain.odomL.getCurrentPosition();
+        odomRCurr = driveTrain.odomR.getCurrentPosition();
+        odomHCurr = driveTrain.odomH.getCurrentPosition();
 
-        currentTicksL = (TotalTicksL - storedTicksL);
-        currentTicksR = (TotalTicksR - storedTicksR);
-        currentTicksB = (TotalTicksB - storedTicksB);
-
-        distanceL = (currentTicksL * some_constant)/dt.seconds();
-        distanceR = (currentTicksR * some_constant)/dt.seconds();
-        distanceB = (currentTicksB * some_constant)/dt.seconds();
-
-        theta = ((distanceL + distanceR)/distBet);
-
-        posDegrees = theta * dt.seconds() + posDegrees;
-        angle = posDegrees*(180/Math.PI);
-
-        velocity_X = ((distanceL + distanceR)/2.0) * Math.cos(theta) - (distanceB - theta) * (2 * Math.sin(theta)); // solves for x distance (contingent on theta being accurate)
-        velocity_Y =((distanceL + distanceR)/2.0) * Math.sin(theta) - (distanceB - theta) * (2 * Math.cos(theta)); // solves for y disance (contingent on theta being accurate)
-
-        global_X = velocity_X * Math.cos(posDegrees) - velocity_Y * Math.sin(posDegrees);
-        global_Y = velocity_X * Math.sin(posDegrees) + velocity_Y * Math.cos(posDegrees);
-
-        pos_X = velocity_X * dt.seconds() + pos_X;
-        pos_Y = velocity_Y * dt.seconds() + pos_Y;
-
-        dt.reset();
+        odomLSpeed = (odomLPrev - odomLCurr)/dt.seconds();
+        odomRSpeed = (odomRPrev - odomRCurr)/dt.seconds();
 
         return false;
     }
