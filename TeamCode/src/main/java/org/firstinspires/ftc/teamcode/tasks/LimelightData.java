41fd445b562582bf85b5d81db1e22c43cc75ceea
rwu60 <ririwu10@gmail.com>
2025-12-18 17:00:28 -0500

    11/11

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tasks/LimelightData.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tasks/LimelightData.java
@@ -6,18 +6,14 @@ import com.qualcomm.hardware.limelightvision.LLResult;
 import com.qualcomm.hardware.limelightvision.LLResultTypes;
 import com.qualcomm.hardware.limelightvision.LLStatus;
 import com.qualcomm.hardware.limelightvision.Limelight3A;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorEx;
 
 import org.firstinspires.ftc.robotcore.external.navigation.Pose3D;
 
 import java.util.List;
 
 @TeleOp(name = "Sensor: Limelight3A", group = "Sensor")
-@Disabled
 class LimelightData extends LinearOpMode {
 
     private Limelight3A limelight;
