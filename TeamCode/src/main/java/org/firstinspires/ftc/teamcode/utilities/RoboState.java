cd39639e667d55382a3dafb93546164795d61873
Noah-MG <noah.glimcher28@avenues.org>
2026-01-25 09:48:29 -0500

    beginnings of more advanced localization and shooting system

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utilities/RoboState.java
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.teamcode.utilities;
+
+public class RoboState {
+    public double x;
+    public double y;
+    public double theta;
+
+    public double VelX;
+    public double VelY;
+    public double VelTheta;
+}
